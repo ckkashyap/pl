bcf834075358e21abd4c7ec4adf6862e76df4348
CK Kashyap <ckkashyap@gmail.com>
2023-09-29 14:52:07 -0700

    Use 3bits for tagging the pointer

---
+++ miniPicoLisp/09012023/test/run.bat
@@ -0,0 +1,13 @@
+@echo off
+
+..\miniPicoLisp\src\picolisp ..\miniPicoLisp\lib.l ..\miniPicoLisp\lib\misc.l src\main_miniPicoLisp.l -bye
+
+..\miniPicoLisp\src\picolisp ..\miniPicoLisp\lib.l ..\miniPicoLisp\lib\misc.l src\apply.l -bye
+
+..\miniPicoLisp\src\picolisp ..\miniPicoLisp\lib.l ..\miniPicoLisp\lib\misc.l src\flow_miniPicoLisp.l -bye
+
+..\miniPicoLisp\src\picolisp ..\miniPicoLisp\lib.l ..\miniPicoLisp\lib\misc.l src\sym_miniPicoLisp.l -bye
+
+..\miniPicoLisp\src\picolisp ..\miniPicoLisp\lib.l ..\miniPicoLisp\lib\misc.l src\io_miniPicoLisp.l -bye
+
+echo ALL TESTS PASSED
